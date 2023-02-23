2082be63702bdc054c20455d27722ab8f7ae9db6
Zachary Pahl <zp3tech@gmail.com>
2023-02-23 00:33:26 -0700

    finished hero background circles effect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,21 @@
 import Head from 'next/head'
 import Header from '@/components/Header'
+import Hero from '@/components/Hero'
 
 export default function Home() {
   return (
-    <>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Beyond the Box Education</title>
       </Head>
-      
+
       {/* { Header } */}
       <Header />
 
       {/* { Hero } */}
+      <section id='hero' className='snap-center'>
+        <Hero />
+      </section>
 
       {/* { About } */}
 
@@ -23,6 +27,6 @@ export default function Home() {
 
       {/* { Contact Me } */}
 
-    </>
+    </div>
   )
 }
