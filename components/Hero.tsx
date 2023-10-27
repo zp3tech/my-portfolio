50536a616d2c020590c33f45fedd2b4276756382
Zachary Pahl <zp3tech@gmail.com>
2023-10-26 23:54:03 -0700

    hero buttons and About section started

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 import BackgroundCircles from './BackgroundCircles';
 import Image from "next/image"
+import Link from 'next/link';
 
 type Props = {}
 
@@ -21,18 +22,26 @@ function Hero({ }: Props) {
       <BackgroundCircles />
       <Image src="logo.svg" height={250} width={250} alt={'Beyond the Box logo'} priority={true} />
 
-      <div>
+      <div className='z-20'>
         <h2 className="text-sm uppercase text-white pb-6 tracking-[10px]">Morgan Bettis-Pahl</h2>
         <h1 className="font-serif text-cyan-300">
           <span>{text}</span>
           <Cursor cursorColor="white" />
         </h1>
 
-        <div>
-          <button className='heroButton'>About</button>
-          <button className='heroButton'>Experience</button>
-          <button className='heroButton'>Skills</button>
-          <button className='heroButton'>Projects</button>
+        <div className='pt-5'>
+          <Link href="#about">
+            <button className='heroButton'>About</button>
+          </Link>
+          <Link href="#experience">
+            <button className='heroButton'>Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className='heroButton'>Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className='heroButton'>Projects</button>
+          </Link>
         </div>
       </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,28 @@
 import Head from 'next/head'
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
+import About from '@/components/About'
 
 export default function Home() {
   return (
     <div className='bg-[rgb(69,69,69)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Beyond the Box Education</title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
       </Head>
 
       {/* { Header } */}
       <Header />
 
       {/* { Hero } */}
-      <section id='hero' className='snap-center'>
+      <section id='hero' className='snap-start'>
         <Hero />
       </section>
 
       {/* { About } */}
+      <section id='about' className='snap-center'>
+        <About />
+      </section>
 
       {/* { Experience } */}
 
