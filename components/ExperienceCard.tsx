8a84d3441ada58dca7a708e7f6202731c1c879b8
Zachary Pahl <zp3tech@gmail.com>
2023-11-16 22:25:30 -0700

    finished ExperienceCard setup for now
    horizontal scroll functions.
    However doesn't look good on mobile... for now.

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -6,7 +6,7 @@ type Props = {}
 
 const ExperienceCard = (props: Props) => {
   return (
-    <article>
+    <article className='flex flex-col rounded-lg items-center space-y-5 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration200 overflow-hidden'>
       <motion.div
         initial={{ y: -100, opacity: 0, }}
         transition={{ duration: 1.2 }}
@@ -24,22 +24,41 @@ const ExperienceCard = (props: Props) => {
 
       <div className='px-0 md:px-10'>
         <h4 className='text-4xl font-light'>Position of experience 1</h4>
-        <p>Experience 1 employer name</p>
-        <div>
+        <p className='font-bold text-2xl mt-1'>Experience 1 employer name</p>
+        <div className='flex space-x-2 my-2'>
           {/* tech used */}
-          {/* tech used */}
-          {/* tech used */}
+          <Image
+            className='h-10 w-10 rounded-full'
+            height={10}
+            width={10}
+            src={"logo.svg"}
+            alt='icon relating to experience 1'
+          />
+          <Image
+            className='h-10 w-10 rounded-full'
+            height={10}
+            width={10}
+            src={"next.svg"}
+            alt='icon relating to experience 1'
+          />
+          <Image
+            className='h-10 w-10 rounded-full'
+            height={10}
+            width={10}
+            src={"/oldfavicon.jpg"}
+            alt='icon relating to experience 1'
+          />
         </div>
-        <p>Started work ... - Ended...</p>
+        <p className='uppercase py-5 text-gray-500'>Started work ... - Ended...</p>
 
         <ul className='list-disc space-y-4 ml-5 text-lg'>
-          <li>Summary points</li>
-          <li>Summary points</li>
-          <li>Summary points</li>
-          <li>Summary points</li>
-          <li>Summary points</li>
-      </ul>
-      
+          <li>First Summary point</li>
+          <li>more Summary points</li>
+          <li>more Summary points</li>
+          <li>more Summary points</li>
+          <li>more Summary points</li>
+        </ul>
+
       </div>
 
     </article>
--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -10,14 +10,13 @@ function WorkExperience({ }: Props) {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+      className='h-screen flex relative overflow-hidden flex-col text-lft md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
 
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
         Experience
       </h3>
 
-      <div>
-        <ExperienceCard />
+      <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory'>
         <ExperienceCard />
         <ExperienceCard />
         <ExperienceCard />
